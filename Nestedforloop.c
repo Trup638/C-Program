d94d00f566082b6cd067d2efc0aa0f33a3612446
Trup638 <truptikare006@gmail.com>
2025-09-02 15:54:11 +0530

    Add files via upload

---
+++ Nestedforloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int i, j;
+
+    for (i = 1; i <= 5; i++) {
+        for (j = 1; j <= 10; j++) {
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
+        printf("----------\n"); // Divider line
+    }
+
+    return 0;
+}
